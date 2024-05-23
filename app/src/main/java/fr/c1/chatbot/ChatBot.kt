e2e3569a12b05f637350b8179776201d84389500
ZPyrolink <corenthin.zozor@gmail.com>
2024-05-23 09:43:44 +0200

    Implemented TTS

--- app/src/main/java/fr/c1/chatbot/ChatBot.kt
+++ app/src/main/java/fr/c1/chatbot/ChatBot.kt
@@ -1,10 +1,14 @@
 package fr.c1.chatbot
 
-import android.app.Application
 import fr.c1.chatbot.model.ActivitiesRepository
 import fr.c1.chatbot.model.Tree
+import fr.c1.chatbot.utils.TTS
+import android.app.Application
 
 class ChatBot : Application() {
+    lateinit var tts: TTS
+        private set
+
     override fun onCreate() {
         super.onCreate()
         val chatbotTree = Tree()
@@ -13,6 +17,7 @@ class ChatBot : Application() {
         chatbotTree.initTree(fileIS)
         val activitiesRepository = ActivitiesRepository()
         activitiesRepository.initAll(this)
-    }
 
+        tts = TTS(this)
+    }
 }

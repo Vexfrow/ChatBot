6a694f37c5f59e12c673f4f38466e64e4765590c
RaphaelBalssa <balssa.raphael@gmail.com>
2024-06-25 13:23:29 +0200

    Ajout info quand on clique(marche pas)

--- app/src/main/java/fr/c1/chatbot/composable/OsmdroidMapView.kt
+++ app/src/main/java/fr/c1/chatbot/composable/OsmdroidMapView.kt
@@ -2,10 +2,14 @@ package fr.c1.chatbot.composable
 
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Point
+import android.widget.TextView
+import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import fr.c1.chatbot.R
 import fr.c1.chatbot.model.activity.AbstractActivity
 import fr.c1.chatbot.model.activity.Association
 import fr.c1.chatbot.model.activity.Building
@@ -25,6 +29,7 @@ import org.osmdroid.views.CustomZoomButtonsController
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.compass.CompassOverlay
 import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider
+import org.osmdroid.views.overlay.infowindow.InfoWindow
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 import org.osmdroid.views.overlay.simplefastpoint.LabelledGeoPoint
@@ -32,6 +37,36 @@ import org.osmdroid.views.overlay.simplefastpoint.SimpleFastPointOverlay
 import org.osmdroid.views.overlay.simplefastpoint.SimpleFastPointOverlayOptions
 import org.osmdroid.views.overlay.simplefastpoint.SimplePointTheme
 
+
+class CustomInfoWindow(private val mapView: MapView) :
+    InfoWindow(R.layout.custom_map_info, mapView) {
+    override fun onOpen(item: Any?) {
+        // Following command
+        //closeAllInfoWindowsOn(mapView)
+
+        val title = mView.findViewById<TextView>(R.id.title)
+        val snippet = mView.findViewById<TextView>(R.id.snippet)
+
+        // Assurez-vous que l'objet item est de type adéquat (ici on suppose qu'il est de type Point)
+        val point = item as LabelledGeoPoint
+
+        title.text = point.label
+
+        // You can set an onClickListener on the InfoWindow itself.
+        // This is so that you can close the InfoWindow once it has been tapped.
+
+        // Instead, you could also close the InfoWindows when the map is pressed.
+        // This is covered in the Map Listeners guide.
+
+        mView.setOnClickListener {
+            close()
+        }
+    }
+
+    override fun onClose() {
+        TODO("Not yet implemented")
+    }
+}
 
 @Composable
 fun OsmdroidMapView(aVM: ActivitiesVM) {
@@ -55,18 +90,19 @@ fun OsmdroidMapView(aVM: ActivitiesVM) {
                 }
                 val mLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(context), this)
                 mLocationOverlay.enableMyLocation()
+                val mapView = this
                 overlays.apply {
                     // create overlays with differents themes
                     // add overlays
                     setResult(aVM.result)
-                    add(setSFPO(festivalsLocations, "#ff4f29"))
-                    add(setSFPO(associationsLocations, "#e478ff"))
-                    add(setSFPO(museesLocations, "#cecece"))
-                    add(setSFPO(expositionsLocations, "#2db0ff"))
-                    add(setSFPO(sitesLocations, "#ffb02d"))
-                    add(setSFPO(contenuLocations, "#fffc93"))
-                    add(setSFPO(edificesLocations, "#b87800"))
-                    add(setSFPO(jardinLocations, "#6cff40"))
+                    add(setSFPO(mapView, festivalsLocations, "#ff4f29"))
+                    add(setSFPO(mapView, associationsLocations, "#e478ff"))
+                    add(setSFPO(mapView, museesLocations, "#cecece"))
+                    add(setSFPO(mapView, expositionsLocations, "#2db0ff"))
+                    add(setSFPO(mapView, sitesLocations, "#ffb02d"))
+                    add(setSFPO(mapView, contenuLocations, "#fffc93"))
+                    add(setSFPO(mapView, edificesLocations, "#b87800"))
+                    add(setSFPO(mapView, jardinLocations, "#6cff40"))
                     add(mLocationOverlay)
                 }
             }
@@ -147,7 +183,7 @@ private fun setResult(res: Resource<List<AbstractActivity>>) {
     }
 }
 
-fun setSFPO(points: ArrayList<IGeoPoint>, color: String): SimpleFastPointOverlay {
+fun setSFPO(mapView : MapView, points: ArrayList<IGeoPoint>, color: String): SimpleFastPointOverlay {
     val textStyle = Paint()
     val pointStyle = Paint()
     textStyle.setTextStyle(color)
@@ -158,7 +194,15 @@ fun setSFPO(points: ArrayList<IGeoPoint>, color: String): SimpleFastPointOverlay
     val opt = SimpleFastPointOverlayOptions.getDefaultStyle()
         .setSymbol(SimpleFastPointOverlayOptions.Shape.CIRCLE)
         .setAlgorithm(SimpleFastPointOverlayOptions.RenderingAlgorithm.MAXIMUM_OPTIMIZATION)
-        .setRadius(7f).setIsClickable(true).setCellSize(15).setTextStyle(textStyle).setPointStyle(textStyle).setMinZoomShowLabels(10)
+        .setRadius(7f).setIsClickable(true).setCellSize(15).setTextStyle(textStyle).setPointStyle(textStyle).setMinZoomShowLabels(15)
+    val sfpo = SimpleFastPointOverlay(pt, opt)
+    // onClick callback
 
-    return SimpleFastPointOverlay(pt, opt)
+    sfpo.setOnClickListener { point, p ->
+        if (point is LabelledGeoPoint) {
+            val infoWindow = CustomInfoWindow(mapView)
+            infoWindow.open(point, point, 0, 0)
+        }
+    }
+    return sfpo
 }

7fad3bb0e612157625ad68b72a32a657f1e55e85
Corenthin <corenthin.zozor@gmail.com>
2024-06-26 12:43:30 +0200

    Rename Compte into Profile

--- app/src/main/java/fr/c1/chatbot/composable/TopBar.kt
+++ app/src/main/java/fr/c1/chatbot/composable/TopBar.kt
@@ -2,6 +2,7 @@ package fr.c1.chatbot.composable
 
 import fr.c1.chatbot.ui.icons.Robot
 import fr.c1.chatbot.ui.theme.ChatBotPrev
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material.icons.Icons
@@ -24,12 +25,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import android.util.Log
-import androidx.compose.foundation.background
-import androidx.compose.ui.graphics.Color
 
 enum class Tab(
     val value: Number,
@@ -42,7 +42,7 @@ enum class Tab(
     ChatBotMap(0.2f, "Carte", Icons.Default.Map),
     Suggestion(1, "Suggestion", Icons.Default.Lightbulb),
     History(2, "Historique", Icons.Default.History),
-    Account(3, "Compte", Icons.Default.AccountCircle),
+    Account(3, "Profile", Icons.Default.AccountCircle),
     AccountData(3.0f, "Données", Icons.Default.AccountCircle),
     AccountPreferences(3.1f, "Préférences hebdomadaire", Icons.Default.DateRange),
     AccountPassions(3.2f, "Centres d'intérêt", Icons.Default.SentimentVerySatisfied),

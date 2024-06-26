9b9d463c75f75974ce8244f534014c843b5e50ee
Corenthin <corenthin.zozor@gmail.com>
2024-06-26 23:55:32 +0200

    Update documentation

--- app/src/main/java/fr/c1/chatbot/model/User.kt
+++ app/src/main/java/fr/c1/chatbot/model/User.kt
@@ -8,16 +8,16 @@ import java.util.UUID
 private const val TAG = "UserProfile"
 
 /**
- * User
- *
- * @property lastName
- * @property firstName
- * @property age
- * @property _cities
- * @property _types
- * @property _passions
- * @property _weeklyPreferences
- * @constructor Create User
+ * Represents a user
+ * @property id Id of the user
+ * @property lastName Name of the user
+ * @property firstName First name of the user
+ * @property age Age of the user
+ * @property _cities List of cities of the user
+ * @property _types List of activity types of the user
+ * @property _passions List of interests of the user
+ * @property _weeklyPreferences List of weekly preferences of the user
+ * @constructor Private: creates a user with all the specified parameters
  */
 class User private constructor(
     val id: String,
@@ -30,6 +30,13 @@ class User private constructor(
     private val _weeklyPreferences: MutableList<WeeklyPreference> = mutableListOf(),
 ) {
     companion object {
+        /**
+         * Parse a [JsonObject] into a [User]
+         *
+         * @param id Id of the parsed user
+         * @param obj [JsonObject] representing the user [id]
+         * @return The parsed user
+         */
         fun from(id: String, obj: JsonObject): User {
             val gson = Gson()
             return User(
@@ -46,6 +53,14 @@ class User private constructor(
             )
         }
 
+        /**
+         * The default user
+         *
+         * - [id] = "01234567-89ab-cdef-0fed-cba987654321"
+         * - [firstName] = "Utilisateur"
+         * - [lastName] = "par défaut"
+         * - [age] = 60
+         */
         val DEFAULT = User(
             id = UUID(0x123456789ABCDEF, 0xFEDCBA987654321).toString(),
             firstName = "Utilisateur",
@@ -54,37 +69,36 @@ class User private constructor(
         )
     }
 
+    /**
+     * Create a new user
+     *
+     * - [id] automatically generated with a [UUID.randomUUID]
+     * - empty [firstName] and [lastName]
+     * - [age] of 60
+     */
     constructor() : this(UUID.randomUUID().toString(), "", "", 60)
 
-    /**
-     * Add city
-     * @param city
-     */
+    /** Add [city] to the [cities] */
     fun addCity(city: String) {
         _cities.add(city)
     }
 
-    /**
-     * Add type
-     * @param type
-     */
+    /** Add an activity [type] to the [types] */
     fun addType(type: Type) {
         _types.add(type)
     }
 
-    /**
-     * Add passion
-     * @param passion
-     */
+    /** Add a [passion] to the [passions] */
     fun addPassion(passion: String) {
         _passions.add(passion)
     }
 
     /**
-     * Add weekly preference
-     * @param day
-     * @param houe
-     * @param duration
+     * Create a new [WeeklyPreference] into the [weeklyPreferences]
+     *
+     * @param day Day of the preference
+     * @param houe Hour of the preference. ToDo: rename into hour
+     * @param duration Duration of the preference
      */
     fun addWeeklyPreference(day: String, houe: String, duration: Int) {
         _weeklyPreferences.add(WeeklyPreference(day, houe, duration))
@@ -92,62 +106,49 @@ class User private constructor(
 
     /**
      * Remove weekly peference
-     * @param day
-     * @param hour
-     * @param duration
+     *
+     * ToDo: This way doesn't work!
+     * Remove by predicte or override the [WeeklyPreference.toString] method
+     *
+     * @param day Day of the preference
+     * @param hour Hour of the preference
+     * @param duration Duration of the preference
      */
     fun removeWeeklyPeference(day: String, hour: String, duration: Int) {
         _weeklyPreferences.remove(WeeklyPreference(day, hour, duration))
     }
 
-    /**
-     * Clear weekly preferences
-     */
+    /** Delete all [weeklyPreferences] */
     fun clearWeeklyPreferences() {
         _weeklyPreferences.clear()
     }
 
-    /**
-     * Remove city
-     * @param city
-     */
+    /** Delete a [city] */
     fun removeCity(city: String) {
         _cities.remove(city)
     }
 
-    /**
-     * Clear cities
-     */
+    /** Delete all [cities] */
     fun clearCities() {
         _cities.clear()
     }
 
-    /**
-     * Remove passion
-     * @param passion
-     */
+    /** Delete a [passion] */
     fun removePassion(passion: String) {
         _passions.remove(passion)
     }
 
-    /**
-     * Clear passions
-     */
+    /** * Clear [passions] */
     fun clearPassions() {
         _passions.clear()
     }
 
-    /**
-     * Remove type
-     * @param type
-     */
+    /** Remove an activity [type] */
     fun removeType(type: Type) {
         _types.remove(type)
     }
 
-    /**
-     * Clear types
-     */
+    /** Delete all [types] */
     fun clearTypes() {
         _types.clear()
     }
@@ -163,10 +164,9 @@ class User private constructor(
     val types: List<Type> get() = _types
 
     /**
-     * Has passion
+     * Check if the [User] have a specified [passion]
      *
-     * @param passion
-     * @return Boolean true if the user has the passion, false otherwise
+     * @return True if the user has the [passion], false otherwise
      */
     fun hasPassion(passion: String): Boolean = passions.contains(passion)
 

4830e25ca4bb7784326367a90c4ce16afcf1c974
Thomas GREGOIRE <thomas.gregoire82@gmail.com>
2024-06-27 10:15:56 +0200

    Message pour expliquer l'absence de certains onglets

--- app/src/main/java/fr/c1/chatbot/composable/History.kt
+++ app/src/main/java/fr/c1/chatbot/composable/History.kt
@@ -1,12 +1,18 @@
 package fr.c1.chatbot.composable
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import fr.c1.chatbot.composable.utils.MyText
 
 @Composable
 fun History(modifier: Modifier = Modifier) =
-    ToDo(name = "Afficher l'historique des précédentes recherches")
+    MyText(
+        text = "Cet onglet contiendra la liste des activités choisis par l'utilisateur\nLe système visant à pouvoir choisir des activités n'étant pas implémenté, cet onglet sert à rien pour le moment",
+        textAlign = TextAlign.Center
+    )
 
 @Preview
 @Composable
